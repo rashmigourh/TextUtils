591ce7491a4caa60d7c90ccb7af6558f9d4fb485
Rashmi Gourh <rashmigourh17@gmail.com>
2023-10-05 09:19:57 +0530

    Updates

---
+++ precache-manifest.12995f00dbcfea79319acbcf6d3039b2.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "81cb8a6297cd3f0d9a93",
+    "url": "/TextUtils/static/css/main.a545bffd.chunk.css"
+  },
+  {
+    "revision": "81cb8a6297cd3f0d9a93",
+    "url": "/TextUtils/static/js/main.250b28aa.chunk.js"
+  },
+  {
+    "revision": "9245c231427987ca1fc9",
+    "url": "/TextUtils/static/js/runtime~main.25f4f17e.js"
+  },
+  {
+    "revision": "e1fb5031da847fc30c19",
+    "url": "/TextUtils/static/js/2.5cc2861d.chunk.js"
+  },
+  {
+    "revision": "50d179a158d504584db6",
+    "url": "/TextUtils/static/js/3.0bdce3fd.chunk.js"
+  },
+  {
+    "revision": "8837f3ea935047eb8ce2eea4da985be4",
+    "url": "/TextUtils/static/media/text2.8837f3ea.png"
+  },
+  {
+    "revision": "2477dc597ec6431cda809152a5ac5bd4",
+    "url": "/TextUtils/static/media/card1.2477dc59.jpg"
+  },
+  {
+    "revision": "9849cc52ae633e8079d9d40c79f28aee",
+    "url": "/TextUtils/static/media/card2.9849cc52.jpg"
+  },
+  {
+    "revision": "ecf8cf4ccbbf74077a6295b2efab3234",
+    "url": "/TextUtils/static/media/card3.ecf8cf4c.jpg"
+  },
+  {
+    "revision": "60a13619730f23abf5800abee3faa774",
+    "url": "/TextUtils/static/media/card4.60a13619.png"
+  },
+  {
+    "revision": "12c82a3beaad258be9060eb572ea2ed7",
+    "url": "/TextUtils/static/media/img1.12c82a3b.jpg"
+  },
+  {
+    "revision": "256dda9b995c1256d99a96e8a55247eb",
+    "url": "/TextUtils/static/media/img2.256dda9b.jpg"
+  },
+  {
+    "revision": "e007365ddba59608ab8e66537933842d",
+    "url": "/TextUtils/static/media/img3.e007365d.jpg"
+  },
+  {
+    "revision": "11c986abf43a66a7d8568c50486999df",
+    "url": "/TextUtils/static/media/text3.11c986ab.png"
+  },
+  {
+    "revision": "160ff3e9ebc96c2337ea307fbed8e852",
+    "url": "/TextUtils/static/media/contact.160ff3e9.png"
+  },
+  {
+    "revision": "185a4ea6a7eed3ff1d6a694798eb8bee",
+    "url": "/TextUtils/index.html"
+  }
+];
